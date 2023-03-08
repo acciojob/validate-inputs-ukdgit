c241278b05d8e81e9fd36764befdc2353190b13f
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-03-08 11:05:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const mobileInput = document.getElementById('mobile');
+
+const namePattern = /^[A-Za-z]{3,}$/;
+const emailPattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
+const mobilePattern = /^[0-9]{10}$/;
+
+nameInput.addEventListener('input', () => {
+  if (namePattern.test(nameInput.value)) {
+    nameInput.style.backgroundColor = 'lightgreen';
+  } else {
+    nameInput.style.backgroundColor = 'pink';
+  }
+});
+
+emailInput.addEventListener('input', () => {
+  if (emailPattern.test(emailInput.value)) {
+    emailInput.style.backgroundColor = 'lightgreen';
+  } else {
+    emailInput.style.backgroundColor = 'pink';
+  }
+});
+
+mobileInput.addEventListener('input', () => {
+  if (mobilePattern.test(mobileInput.value)) {
+    mobileInput.style.backgroundColor = 'lightgreen';
+  } else {
+    mobileInput.style.backgroundColor = 'pink';
+  }
+});
